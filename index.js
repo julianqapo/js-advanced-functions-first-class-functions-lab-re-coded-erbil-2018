4e8d06405b04d69c026957c44bc1e89ddf120de0
Julian M. Qapo <julian.qapo@gmail.com>
2019-01-13 11:45:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(y){
+  const x = function(){
+return y.slice(0,2)
+  }
+return x()
+}
+
+
+function returnLastTwoDrivers(y){
+  function x(){
+    return y.slice(-2)
+  }
+  return x()
+}
+
+
+const selectingDrivers = [['ant', 'bison'],['duck', 'elephant']]
